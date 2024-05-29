e2b109f6b996106426e5518696042bd4a6a90db8
julia <juliakofmana@gmail.com>
2024-05-29 23:57:59 +0300

    app_electronics serializers edit

--- electronics/serializers/contacts.py
+++ electronics/serializers/contacts.py
@@ -9,3 +9,12 @@ class ContactsSerializers(serializers.ModelSerializer):
     class Meta:
         model = Contacts
         fields = '__all__'
+        extra_kwargs = {
+            'creation_user': {'required': False}  # делаем поле creation_user необязательным
+        }
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        if request and request.user:
+            validated_data['creation_user'] = request.user
+        return super().create(validated_data)
--- electronics/views/contacts.py
+++ electronics/views/contacts.py
@@ -11,3 +11,8 @@ class ContactsViewSet(ModelViewSet):
     queryset = Contacts.objects.all()
     permission_classes = [IsAuthenticated]
     pagination_class = ElectronicsPagination
+
+    def perform_create(self, serializer):
+        new_obj = serializer.save()
+        new_obj.creation_user = self.request.user
+        new_obj.save()
