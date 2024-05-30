b427a2fc64f8ca43728f01a5ba212f6bb69ec8ba
julia <juliakofmana@gmail.com>
2024-05-30 22:28:00 +0300

    app_electronics

--- electronics/views/product.py
+++ electronics/views/product.py
@@ -4,15 +4,24 @@ from rest_framework.viewsets import ModelViewSet
 from electronics.models import Product
 from electronics.paginators import ElectronicsPagination
 from electronics.serializers.product import ProductSerializers
+from users.permissions import IsOwner, IsModerator, IsSuperUser
 
 
 class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializers
     queryset = Product.objects.all()
-    permission_classes = [IsAuthenticated]
     pagination_class = ElectronicsPagination
 
     def perform_create(self, serializer):
         new_obj = serializer.save()
         new_obj.creation_user = self.request.user
         new_obj.save()
+
+    def get_permissions(self):
+        if self.action in ['create', 'list', 'retrieve']:
+            self.permission_classes = [IsAuthenticated]
+        elif self.action in ['update', 'partial_update']:
+            self.permission_classes = [IsAuthenticated, IsOwner | IsModerator | IsSuperUser]
+        elif self.action == 'destroy':
+            self.permission_classes = [IsAuthenticated, IsOwner | IsSuperUser]
+        return [permission() for permission in self.permission_classes]
