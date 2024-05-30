4f711a81451b9fe813b3dea60ca0788532f568f7
julia <juliakofmana@gmail.com>
2024-05-30 22:20:39 +0300

    app_electronics

--- electronics/views/contacts.py
+++ electronics/views/contacts.py
@@ -4,15 +4,24 @@ from rest_framework.viewsets import ModelViewSet
 from electronics.models import Contacts
 from electronics.paginators import ElectronicsPagination
 from electronics.serializers.contacts import ContactsSerializers
+from users.permissions import IsOwner, IsModerator, IsSuperUser
 
 
 class ContactsViewSet(ModelViewSet):
     serializer_class = ContactsSerializers
     queryset = Contacts.objects.all()
-    permission_classes = [IsAuthenticated]
     pagination_class = ElectronicsPagination
 
     def perform_create(self, serializer):
         new_obj = serializer.save()
         new_obj.creation_user = self.request.user
         new_obj.save()
+
+    def get_permissions(self):
+        if self.action in ['create', 'list', 'retrieve']:
+            self.permission_classes = [IsAuthenticated]
+        elif self.action in ['update', 'partial_update']:
+            self.permission_classes = [IsAuthenticated, IsOwner | IsModerator | IsSuperUser]
+        elif self.action == 'destroy':
+            self.permission_classes = [IsAuthenticated, IsOwner | IsSuperUser]
+        return [permission() for permission in self.permission_classes]
