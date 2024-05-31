3dd19efb5f7c83fa497aac02c04cad451373c463
julia <juliakofmana@gmail.com>
2024-05-31 21:44:34 +0300

    validators

--- electronics/validators.py
+++ electronics/validators.py
@@ -7,26 +7,31 @@ class SupplierValidator:
     def __call__(self, value):
         if value.get('network_type') == 0 and value.get('supplier_name'):
             raise ValidationError('У завода не может быть поставщика. '
-                                  'Выберете корректный тип сети или удалите поставщика'
+                                  'Выберете корректный тип сети или удалите поставщика.'
+                                  )
+
+        if value.get('network_type') != 0 and value.get('level') == 0 and value.get('supplier_name') != 0:
+            raise ValidationError('На нулевом уровне поставки может находиться только завод. '
+                                  'Выберете корректный тип сети или удалите поставщика.'
                                   )
 
         if value.get('supplier_name') and value.get('level') is not None:
             if value.get('level') != value['supplier_name'].level + 1:
                 raise ValidationError(
                     'Уровень поставщика должен быть на 1 выше уровня вашего поставщика. '
-                    'Вы можете не выставлять уровень вручную, программа сделает это автоматически'
+                    'Вы можете не выставлять уровень вручную, программа сделает это автоматически.'
                 )
 
         if value.get('level') is None and value.get('supplier_name') is None and value.get('network_type') is None:
             raise ValidationError(
                 'Для создания поставщика укажите поставщика и тип сети. '
-                'Если вы являетесь заводом, укажите тип сети - 0'
+                'Если вы являетесь заводом, укажите тип сети - 0.'
             )
 
         if value.get('network_type') != 0 and value.get('level') is None and value.get('supplier_name') is None:
             raise ValidationError(
                 'Для создания записи укажите вашего поставщика. '
-                'Если вы являетесь заводом, укажите тип сети - 0'
+                'Если вы являетесь заводом, укажите тип сети - 0.'
             )
 
         if value.get('network_type') == 0 and value.get('level') != 0 and value.get('level') is not None:
@@ -42,3 +47,8 @@ class SupplierValidator:
             raise ValidationError(
                 'Для создания поставщика укажите тип вашей сети.'
             )
+
+        if value.get('level') == 0 and value.get('network_type') != 0:
+            raise ValidationError('На нулевом уровне поставки может быть только завод. '
+                                  'Если вы являетесь заводом, укажите тип сети - 0.'
+                                  )
