bc2b94d27e0a8202086486444fb81f240830ea6d
julia <juliakofmana@gmail.com>
2024-05-31 21:48:49 +0300

    admin

--- electronics/admin.py
+++ electronics/admin.py
@@ -5,21 +5,28 @@ from .models import Contacts, Product, Supplier
 from .serializers.supplier import SupplierCreateSerializers
 
 
+@admin.action(description='Очистить задолженность')
+def set_null_debt(ModelAdmin, request, queryset):
+    """ Действие, очищающее задолженность (меняет поле debt=0). """
+    queryset.update(debt=0)
+
+
 @admin.register(Contacts)
 class ContactsAdmin(admin.ModelAdmin):
-    list_display = ('email', 'country', 'city', 'street', 'house_number')
+    list_display = ('id', 'email', 'country', 'city', 'street', 'house_number', 'creation_user')
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_title', 'product_model', 'release_date', 'price')
+    list_display = ('id', 'product_title', 'product_model', 'release_date', 'price', 'creation_user')
 
 
 @admin.register(Supplier)
 class SupplierAdmin(admin.ModelAdmin):
-    list_display = ('name', 'network_type', 'level', 'contact', 'product', 'supplier_name', 'debt', 'creation_time',
-                    'creation_user')
+    list_display = ('id', 'name', 'network_type', 'level', 'contact', 'product', 'supplier_name', 'debt',
+                    'creation_time', 'creation_user')
     list_filter = ('contact__city', 'contact__country')
+    actions = [set_null_debt]
 
     def save_model(self, request, obj, form, change):
         serializer = SupplierCreateSerializers(data=request.POST)
